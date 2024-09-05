4274ff80a7e1642891f759c0b257e59404703414
Vinay-UI-ai <vinay.patel@innovestigia.in>
2024-09-05 17:28:07 +0530

    ..
    ..

--- icp-1/src/Component/Demand/DemandT.jsx
+++ icp-1/src/Component/Demand/DemandT.jsx
@@ -152,7 +152,7 @@ const App = () => {
         rowSelection={rowSelection}
         columns={columns}
         dataSource={data}
-        scroll={{ y: 440 }}  // Set the height of the table here
+        scroll={{ y: 400 }}  // Set the height of the table here
       />
       <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '16px'  }}>
         <Button type="primary"  icon={<ArrowLeftOutlined />}
